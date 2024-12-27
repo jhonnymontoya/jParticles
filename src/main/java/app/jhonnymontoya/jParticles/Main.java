59c5a94f72b97705004aa189f41996259c5aa219
Jhonny Montoya <jhonnymontoya@gmail.com>
2024-12-27 01:56:11 -0500

    Adds main class - entry point
    Signed-off-by: Jhonny Montoya <jhonnymontoya@gmail.com>

---
+++ src/main/java/app/jhonnymontoya/jParticles/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package app.jhonnymontoya.jParticles;
+
+/**
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
